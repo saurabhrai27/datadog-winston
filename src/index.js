12bb671e750f01b6987f8616f00e36895c6aa741
Saurabh Rai <saurabhrai.cec@gmail.com>
2024-04-27 14:09:45 +0530

    logger module with custom transports for datadog

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import logger from './logger';
+import logger from './logger.js';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
+    logger.info('Testing the logs')
     res.send({message: 'Hello'})
 })
 
 app.listen(PORT, () => {
     logger.info('App is listening on port:' + PORT);
-    logger.error('Error log',new Error('Testing the logger'));
 })
